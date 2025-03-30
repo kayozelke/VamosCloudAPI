be053c37be856ebdee5abf79fcc966082d6254bf
kayoz <dawid.kaj09@gmail.com>
2025-03-30 15:15:07 +0200

    update app.py and database_module.py

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy as db
-import database_module as db
+from database_module import init_db, TrackDb
 
 app = Flask(__name__)
 
 # Database configuration (using MySQL from config)
-config = {
-    'DATABASE': {
-        'username': 'flask_test',
-        'password': 'flask_test',
-        'host': '127.0.0.1',
-        'port': 3306,
-        'database': 'vamos_cloud_app'
-    }
+app.config['DATABASE'] = {
+    'username': 'flask_test',
+    'password': 'flask_test',
+    'host': '127.0.0.1',
+    'port': 3306,
+    'database': 'vamos_cloud_app'
 }
 
-
-
-
-# Create tables if they do not exist
-with app.app_context():
-    db_engine = db.get_engine_from_config(config)
-    db.check_and_create_tables(engine=db_engine)
-
-# -------------------- GET all tracks --------------------
-@app.route('/objects', methods=['GET'])
-def get_tracks():
-    return jsonify(db.get_tracks(engine=db_engine))
+# Inicjalizujemy SQLAlchemy i model TrackDb
+db_sqlalchemy = init_db(app)
 
 # -------------------- GET track by song name --------------------
 @app.route('/object', methods=['GET'])
@@ -36,15 +22,46 @@ def get_track_by_name():
     if not name:
         return jsonify({"error": "Missing 'name' parameter"}), 400
     
-    track = db.TrackDb.query.filter_by(song=name).first()
+    track = TrackDb.query.filter_by(song=name).first()
+    
     if track:
-        return jsonify({ "id": track.id, "song": track.song, "artist": track.artist, "streams": track.streams, 
-                            "daily_streams": track.daily_streams, "genre": track.genre, "release_year": track.release_year,
-                            "peak_position": track.peak_position, "weeks_on_chart": track.weeks_on_chart,
-                            "lyrics_sentiment": track.lyrics_sentiment, "tiktok_virality": track.tiktok_virality,
-                            "danceability": track.danceability, "acousticness": track.acousticness, "energy": track.energy })
+        return jsonify({ 
+            "id": track.id, "song": track.song, "artist": track.artist, "streams": track.streams, 
+            "daily_streams": track.daily_streams, "genre": track.genre, "release_year": track.release_year,
+            "peak_position": track.peak_position, "weeks_on_chart": track.weeks_on_chart,
+            "lyrics_sentiment": track.lyrics_sentiment, "tiktok_virality": track.tiktok_virality,
+            "danceability": track.danceability, "acousticness": track.acousticness, "energy": track.energy 
+        })
     else:
         return jsonify({"error": "Track not found"}), 404
+
+
+# # Create tables if they do not exist
+# with app.app_context():
+#     db_engine = db.get_engine_from_config(config)
+#     db.check_and_create_tables(engine=db_engine)
+
+# # -------------------- GET all tracks --------------------
+# @app.route('/objects', methods=['GET'])
+# def get_tracks():
+#     return jsonify(db.get_tracks(engine=db_engine))
+
+# # -------------------- GET track by song name --------------------
+# @app.route('/object', methods=['GET'])
+# def get_track_by_name():
+#     name = request.args.get('name')
+#     if not name:
+#         return jsonify({"error": "Missing 'name' parameter"}), 400
+    
+#     track = db.TrackDb.query.filter_by(song=name).first()
+#     if track:
+#         return jsonify({ "id": track.id, "song": track.song, "artist": track.artist, "streams": track.streams, 
+#                             "daily_streams": track.daily_streams, "genre": track.genre, "release_year": track.release_year,
+#                             "peak_position": track.peak_position, "weeks_on_chart": track.weeks_on_chart,
+#                             "lyrics_sentiment": track.lyrics_sentiment, "tiktok_virality": track.tiktok_virality,
+#                             "danceability": track.danceability, "acousticness": track.acousticness, "energy": track.energy })
+#     else:
+#         return jsonify({"error": "Track not found"}), 404
 
 
 # # -------------------- POST create new track --------------------
--- database_module.py
+++ database_module.py
@@ -1,77 +1,77 @@
-
 import sqlalchemy as db
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
-db_base = declarative_base()
+db_sqlalchemy = SQLAlchemy()
 
 # tables
-class TrackDb(db_base):
+class TrackDb(db_sqlalchemy.Model):
     __tablename__ = 'tracks'
-    # data is like below
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    song = db.Column(db.String(255), nullable=False)
-    artist = db.Column(db.String(255), nullable=False)
-    streams = db.Column(db.BigInteger, nullable=False)
-    daily_streams = db.Column(db.Integer, nullable=False)
-    genre = db.Column(db.String(100), nullable=False)
-    release_year = db.Column(db.Integer, nullable=False)
-    peak_position = db.Column(db.Integer, nullable=False)
-    weeks_on_chart = db.Column(db.Integer, nullable=False)
-    lyrics_sentiment = db.Column(db.Float, nullable=False)
-    tiktok_virality = db.Column(db.Integer, nullable=False)
-    danceability = db.Column(db.Float, nullable=False)
-    acousticness = db.Column(db.Float, nullable=False)
-    energy = db.Column(db.Float, nullable=False)
+    
+    id = db_sqlalchemy.Column(db.Integer, primary_key=True, autoincrement=True)
+    song = db_sqlalchemy.Column(db_sqlalchemy.String(255), nullable=False)
+    artist = db_sqlalchemy.Column(db_sqlalchemy.String(255), nullable=False)
+    streams = db_sqlalchemy.Column(db_sqlalchemy.BigInteger, nullable=False)
+    daily_streams = db_sqlalchemy.Column(db_sqlalchemy.Integer, nullable=False)
+    genre = db_sqlalchemy.Column(db_sqlalchemy.String(100), nullable=False)
+    release_year = db_sqlalchemy.Column(db_sqlalchemy.Integer, nullable=False)
+    peak_position = db_sqlalchemy.Column(db_sqlalchemy.Integer, nullable=False)
+    weeks_on_chart = db_sqlalchemy.Column(db_sqlalchemy.Integer, nullable=False)
+    lyrics_sentiment = db_sqlalchemy.Column(db_sqlalchemy.Float, nullable=False)
+    tiktok_virality = db_sqlalchemy.Column(db_sqlalchemy.Integer, nullable=False)
+    danceability = db_sqlalchemy.Column(db_sqlalchemy.Float, nullable=False)
+    acousticness = db_sqlalchemy.Column(db_sqlalchemy.Float, nullable=False)
+    energy = db_sqlalchemy.Column(db_sqlalchemy.Float, nullable=False)
 
-def get_engine(username : str, password : str, host : str, port : int, database : str):
-    return db.create_engine(
-        url = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
-    )
+
+def init_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{app.config['DATABASE']['username']}:{app.config['DATABASE']['password']}@{app.config['DATABASE']['host']}:{app.config['DATABASE']['port']}/{app.config['DATABASE']['database']}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-def get_engine_from_config(config: dict):
-    return get_engine(
-        username = config['DATABASE']['username'],
-        password = config['DATABASE']['password'],
-        host     = config['DATABASE']['host'],
-        port     = config['DATABASE']['port'],
-        database = config['DATABASE']['database']
-    )
+    db_sqlalchemy.init_app(app)
+
+    with app.app_context():
+        db_sqlalchemy.create_all()
+
+    return db_sqlalchemy
+
+
+
+# def create_tables(engine, db_base = db_base):
+#     db_base.metadata.create_all(engine)   
+#     print("Tables created.")
     
-def create_tables(engine, db_base = db_base):
-    db_base.metadata.create_all(engine)   
-    print("Tables created.")
-    
-def check_and_create_tables(engine):
-    inspector = db.inspect(engine)
-    if 'tracks' not in inspector.get_table_names():
-        create_tables(engine)
+# def check_and_create_tables(engine):
+#     inspector = db.inspect(engine)
+#     if 'tracks' not in inspector.get_table_names():
+#         create_tables(engine)
         
         
-def get_tracks(engine):
-    Session = db.orm.sessionmaker(bind=engine)
-    session = Session()
-    tracks = session.query(TrackDb).all()
-    session.close()
+# def get_tracks(engine):
+#     Session = db.orm.sessionmaker(bind=engine)
+#     session = Session()
+#     tracks = session.query(TrackDb).all()
+#     session.close()
     
-    result = []
-    for track in tracks:
-        result.append({
-            'id': track.id,
-            'song': track.song,
-            'artist': track.artist,
-            'streams': track.streams,
-            'daily_streams': track.daily_streams,
-            'genre': track.genre,
-            'release_year': track.release_year,
-            'peak_position': track.peak_position,
-            'weeks_on_chart': track.weeks_on_chart,
-            'lyrics_sentiment': track.lyrics_sentiment,
-            'tiktok_virality': track.tiktok_virality,
-            'danceability': track.danceability,
-            'acousticness': track.acousticness,
-            'energy': track.energy
-        })
-    return result
+#     result = []
+#     for track in tracks:
+#         result.append({
+#             'id': track.id,
+#             'song': track.song,
+#             'artist': track.artist,
+#             'streams': track.streams,
+#             'daily_streams': track.daily_streams,
+#             'genre': track.genre,
+#             'release_year': track.release_year,
+#             'peak_position': track.peak_position,
+#             'weeks_on_chart': track.weeks_on_chart,
+#             'lyrics_sentiment': track.lyrics_sentiment,
+#             'tiktok_virality': track.tiktok_virality,
+#             'danceability': track.danceability,
+#             'acousticness': track.acousticness,
+#             'energy': track.energy
+#         })
+#     return result
 
     
     
